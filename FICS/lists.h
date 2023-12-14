b928e98f7415fa2b8d65b5dff186c69522600e66
Markus Uhlin <markus@nifty-networks.net>
2023-12-15 00:29:57 +0100

    Reformatted what was left

--- FICS/lists.h
+++ FICS/lists.h
@@ -1,22 +1,51 @@
-/* lists.h */
+/* lists.h
+ *
+ * Revised by maxxe 23/12/15
+ */
 
 #include <stdio.h>
 
 #ifndef _LISTS_H
 #define _LISTS_H
 
-/* yes, it's all cheesy..  there is no significance to the order, but make
-   sure it matches the order in lists.c */
+enum ListWhich {
+	L_ADMIN = 0,
+	L_REMOVEDCOM,
+	L_FILTER,
+	L_BAN,
+	L_ABUSER,
+	L_MUZZLE,
+	L_CMUZZLE,
+	L_FM,
+	L_IM,
+	L_GM,
+	L_BLIND,
+	L_TEAMS,
+	L_COMPUTER,
+	L_TD,
+	L_CENSOR,
+	L_GNOTIFY,
+	L_NOPLAY,
+	L_NOTIFY,
+	L_CHANNEL
+};
 
-enum ListWhich {L_ADMIN = 0, L_REMOVEDCOM, L_FILTER, L_BAN, L_ABUSER,
-L_MUZZLE, L_CMUZZLE, L_FM, L_IM, L_GM, L_BLIND, L_TEAMS, L_COMPUTER,
-L_TD, L_CENSOR, L_GNOTIFY, L_NOPLAY, L_NOTIFY, L_CHANNEL};
+enum ListPerm {
+	P_HEAD = 0,
+	P_GOD,
+	P_ADMIN,
+	P_PUBLIC,
+	P_PERSONAL
+};
 
-enum ListPerm {P_HEAD = 0, P_GOD, P_ADMIN, P_PUBLIC, P_PERSONAL};
+typedef struct {
+	enum ListPerm	 rights;
+	char		*name;
+} ListTable;
 
-typedef struct {enum ListPerm rights; char *name;} ListTable;
-
-/* max. names in one list: 200 */
+/*
+ * Max names in one list.
+ */
 #define MAX_GLOBAL_LIST_SIZE 200
 
 typedef struct _List List;
