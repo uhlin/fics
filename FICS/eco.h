cc7387a07324e19cdd65107b5ba46ef4340c6c09
Markus Uhlin <markus@nifty-networks.net>
2023-12-16 18:11:07 +0100

    Sorted the function declarations

--- FICS/eco.h
+++ FICS/eco.h
@@ -2,11 +2,11 @@
 #define _ECO_H
 
 extern char *boardToFEN();
-extern void ECO_init();
-extern void NIC_init();
-extern void LONG_init();
-extern void BookInit();
-extern int com_eco();
 extern char *getECO();
+extern int com_eco();
+extern void BookInit();
+extern void ECO_init();
+extern void LONG_init();
+extern void NIC_init();
 
 #endif
