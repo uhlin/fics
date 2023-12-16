d0dafa0f71f13c4724a4b62a5fcb56fdf246d5d9
Markus Uhlin <markus@nifty-networks.net>
2023-12-16 18:12:04 +0100

    Added alignment

--- FICS/eco.h
+++ FICS/eco.h
@@ -1,12 +1,12 @@
 #ifndef _ECO_H
 #define _ECO_H
 
-extern char *boardToFEN();
-extern char *getECO();
-extern int com_eco();
-extern void BookInit();
-extern void ECO_init();
-extern void LONG_init();
-extern void NIC_init();
+extern char	*boardToFEN();
+extern char	*getECO();
+extern int	 com_eco();
+extern void	 BookInit();
+extern void	 ECO_init();
+extern void	 LONG_init();
+extern void	 NIC_init();
 
 #endif
