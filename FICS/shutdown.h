1d589e998c636249e4082a6688a142aa547cc4bf
Markus Uhlin <markus@nifty-networks.net>
2023-12-18 22:27:49 +0100

    Added argument lists

--- FICS/shutdown.h
+++ FICS/shutdown.h
@@ -3,12 +3,14 @@
 #ifndef _SHUTDOWN_H
 #define _SHUTDOWN_H
 
+#include "command.h" /* param_list */
+
 extern int check_and_print_shutdown(int);
-extern int com_shutdown();
-extern int com_whenshut();
-extern int server_shutdown();
-extern void ShutDown();
-extern void ShutHeartBeat();
-extern void output_shut_mess();
+extern int com_shutdown(int, param_list);
+extern int com_whenshut(int, param_list);
+extern int server_shutdown(int, char *);
+extern void ShutDown(void);
+extern void ShutHeartBeat(void);
+extern void output_shut_mess(void);
 
 #endif /* _SHUTDOWN_H */
