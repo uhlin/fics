e69f784c48dd2a7b6167c8d6cdeddba3cbed866a
Markus Uhlin <markus@nifty-networks.net>
2023-12-18 22:23:35 +0100

    Sorted the function declarations

--- FICS/shutdown.h
+++ FICS/shutdown.h
@@ -3,12 +3,12 @@
 #ifndef _SHUTDOWN_H
 #define _SHUTDOWN_H
 
-extern void output_shut_mess();
-extern int com_shutdown();
-extern void ShutHeartBeat();
-extern void ShutDown();
-extern int server_shutdown();
 extern int check_and_print_shutdown(int);
+extern int com_shutdown();
 extern int com_whenshut();
+extern int server_shutdown();
+extern void ShutDown();
+extern void ShutHeartBeat();
+extern void output_shut_mess();
 
 #endif /* _SHUTDOWN_H */
