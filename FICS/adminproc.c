666df0c1b0b8c978e86d69042efd6c70ddc703af
Markus Uhlin <markus@nifty-networks.net>
2024-05-09 20:39:57 +0200

    Reformatted com_asetblitz()

--- FICS/adminproc.c
+++ FICS/adminproc.c
@@ -1509,22 +1509,25 @@ PRIVATE void SetRating(int p1, param_list param, statistics *s)
  *   The parameters are self-explanatory: rating, # of wins, # of losses,
  *   # of draws, and ratings deviation.
  */
-PUBLIC int com_asetblitz(int p, param_list param)
+PUBLIC int
+com_asetblitz(int p, param_list param)
 {
-  int p1, connected;
+	int	p1, connected;
 
-  ASSERT(parray[p].adminLevel >= ADMIN_ADMIN);
-  if (!FindPlayer(p, param[0].val.word, &p1, &connected))
-    return COM_OK;
+	ASSERT(parray[p].adminLevel >= ADMIN_ADMIN);
 
-  SetRating(p1, param, &parray[p1].b_stats);
-  player_save(p1);
-  UpdateRank(TYPE_BLITZ, parray[p1].name, &parray[p1].b_stats,
-	     parray[p1].name);
-  pprintf(p, "Blitz rating for %s modified.\n", parray[p1].name);
-  if (!connected)
-    player_remove(p1);
-  return COM_OK;
+	if (!FindPlayer(p, param[0].val.word, &p1, &connected))
+		return COM_OK;
+
+	SetRating(p1, param, &parray[p1].b_stats);
+	player_save(p1);
+	UpdateRank(TYPE_BLITZ, parray[p1].name, &parray[p1].b_stats,
+	    parray[p1].name);
+	pprintf(p, "Blitz rating for %s modified.\n", parray[p1].name);
+
+	if (!connected)
+		player_remove(p1);
+	return COM_OK;
 }
 
 /*
