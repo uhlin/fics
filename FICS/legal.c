39f350402be6a736b9ce84acdce7d130fa0bd590
Markus Uhlin <markus@nifty-networks.net>
2024-06-01 18:28:10 +0200

    Line joining

--- FICS/legal.c
+++ FICS/legal.c
@@ -24,17 +24,16 @@
 
 #include "legal.h"
 
-char legalNotice[] = "\n\
-    fics - An internet chess server.\n\
-    Copyright (C) 1993  Richard V. Nash\n\
-\n\
-    This program is free software; you can redistribute it and/or modify\n\
-    it under the terms of the GNU General Public License as published by\n\
-    the Free Software Foundation; either version 2 of the License, or\n\
-    (at your option) any later version.\n\
-\n\
-    This program is distributed in the hope that it will be useful,\n\
-    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
-    GNU General Public License for more details.\n\
-";
+char legalNotice[] =
+  "\nfics - An internet chess server.\n"
+  "Copyright (C) 1993  Richard V. Nash\n"
+  "\n"
+  "This program is free software; you can redistribute it and/or modify\n"
+  "it under the terms of the GNU General Public License as published by\n"
+  "the Free Software Foundation; either version 2 of the License, or\n"
+  "(at your option) any later version.\n"
+  "\n"
+  "This program is distributed in the hope that it will be useful,\n"
+  "but WITHOUT ANY WARRANTY; without even the implied warranty of\n"
+  "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n"
+  "GNU General Public License for more details.\n";
