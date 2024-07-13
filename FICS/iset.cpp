1bbff988b943931f1357e95f1c7c4c2c9bd6e0b4
Markus Uhlin <markus@nifty-networks.net>
2024-07-13 14:01:29 +0200

    Added code

--- FICS/iset.cpp
+++ FICS/iset.cpp
@@ -2,11 +2,19 @@
 #include "common.h"
 
 #include "iset.h"
+#include "utils.h"
 
 PUBLIC int
 com_iset(int p, param_list param)
 {
-	(void) p;
-	(void) param;
+	if (param[0].type != TYPE_WORD ||
+	    param[1].type != TYPE_WORD) {
+		pprintf(p, "%s: bad parameters\n", __func__);
+		return COM_BADPARAMETERS;
+	}
+
+	pprintf(p, "%s: %s %s\n", __func__,
+	    param[0].val.word,
+	    param[1].val.word);
 	return COM_OK;
 }
