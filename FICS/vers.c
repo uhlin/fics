fbdca8cfe42c178f8763126224ee5404b9b6ce68
Markus Uhlin <markus@nifty-networks.net>
2024-11-23 11:34:36 +0100

    Made vars const

--- FICS/vers.c
+++ FICS/vers.c
@@ -1,5 +1,5 @@
 #include "vers.h"
 
-char SGS_VERS[] = "";
-char VERS_NUM[] = "rpblc-1.4.3";
-char COMP_DATE[] = __DATE__;
+const char SGS_VERS[] = "";
+const char VERS_NUM[] = "rpblc-1.4.3";
+const char COMP_DATE[] = __DATE__;
