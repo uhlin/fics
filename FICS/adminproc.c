494de81056a0fca9fe00cc855b1e487545c7d6ee
Markus Uhlin <markus@nifty-networks.net>
2025-01-26 11:31:20 +0100

    Made the header layout look like everywhere else

--- FICS/adminproc.c
+++ FICS/adminproc.c
@@ -1,17 +1,17 @@
 /*
- * fics - An internet chess server
- * Copyright (c) 1993 Richard V. Nash
- *
- * This program is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License as published by the Free Software
- * Foundation; either version 2 of the License, or (at your option) any later
- * version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
- * details.
- */
+    fics - An internet chess server.
+    Copyright (C) 1993  Richard V. Nash
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+*/
 
 #include "stdinclude.h"
 #include "common.h"
