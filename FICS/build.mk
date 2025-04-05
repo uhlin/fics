1f4f3c2ba2a0474e16fd164536fd168afa27addd
Markus Uhlin <markus@nifty-networks.net>
2025-04-05 23:55:59 +0200

    Added new var 'SRCS'

--- FICS/build.mk
+++ FICS/build.mk
@@ -37,6 +37,40 @@ OBJS = $(SRC_DIR)adminproc.o\
 	$(SRC_DIR)variable.o\
 	$(SRC_DIR)vers.o
 
+SRCS = $(SRC_DIR)adminproc.c\
+	$(SRC_DIR)algcheck.c\
+	$(SRC_DIR)assert_error.c\
+	$(SRC_DIR)board.c\
+	$(SRC_DIR)command.c\
+	$(SRC_DIR)comproc.c\
+	$(SRC_DIR)eco.c\
+	$(SRC_DIR)fics_getsalt.cpp\
+	$(SRC_DIR)ficslim.cpp\
+	$(SRC_DIR)ficsmain.c\
+	$(SRC_DIR)formula.c\
+	$(SRC_DIR)gamedb.c\
+	$(SRC_DIR)gameproc.c\
+	$(SRC_DIR)iset.cpp\
+	$(SRC_DIR)legal.c\
+	$(SRC_DIR)legal2.c\
+	$(SRC_DIR)lists.c\
+	$(SRC_DIR)matchproc.c\
+	$(SRC_DIR)maxxes-utils.c\
+	$(SRC_DIR)movecheck.c\
+	$(SRC_DIR)multicol.c\
+	$(SRC_DIR)network.c\
+	$(SRC_DIR)obsproc.c\
+	$(SRC_DIR)playerdb.c\
+	$(SRC_DIR)rating_conv.c\
+	$(SRC_DIR)ratings.c\
+	$(SRC_DIR)rmalloc.c\
+	$(SRC_DIR)shutdown.c\
+	$(SRC_DIR)sought.cpp\
+	$(SRC_DIR)talkproc.c\
+	$(SRC_DIR)utils.c\
+	$(SRC_DIR)variable.c\
+	$(SRC_DIR)vers.c
+
 AP_OBJS = $(SRC_DIR)fics_addplayer.o
 
 MR_OBJS = $(SRC_DIR)makerank.o
