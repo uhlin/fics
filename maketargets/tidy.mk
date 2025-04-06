eb9a33663a302f10e55d25800a06ed0c872d1a54
Markus Uhlin <markus@nifty-networks.net>
2025-04-06 11:42:57 +0200

    Added new var 'FICS_CLANG_TIDYFLAGS'

--- maketargets/tidy.mk
+++ maketargets/tidy.mk
@@ -2,7 +2,8 @@
 
 TIDY = clang-tidy
 TIDYFLAGS = -checks=-clang-analyzer-security.insecureAPI.strcpy,-clang-analyzer-optin.performance.Padding,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling -quiet
+FICS_CLANG_TIDYFLAGS ?=
 
 tidy: $(INCLUDE_DIR)ficspaths.h
-	$(E) "  TIDY"
-	$(TIDY) $(SRCS) $(TIDYFLAGS) -- -I $(INCLUDE_DIR) $(CPPFLAGS)
+	$(TIDY) $(SRCS) $(TIDYFLAGS) $(FICS_CLANG_TIDYFLAGS) -- \
+	-I $(INCLUDE_DIR) $(CPPFLAGS)
