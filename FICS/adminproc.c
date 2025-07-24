c7cde29d436eeac3dd88f8fa458088a56ea3f11e
Markus Uhlin <markus@nifty-networks.net>
2025-07-24 02:01:38 +0200

    com_checkPLAYER: aligned variables and values

--- FICS/adminproc.c
+++ FICS/adminproc.c
@@ -613,18 +613,22 @@ com_checkPLAYER(int p, param_list param)
 		pprintf(p, "%s is not logged in.\n", v_player);
 		stolower(v_player);
 
-		pprintf(p, "name = %s\n", parray[p1].name);
-		pprintf(p, "login = %s\n", parray[p1].login);
-		pprintf(p, "fullName = %s\n", (parray[p1].fullName ?
-		    parray[p1].fullName : "(none)"));
-		pprintf(p, "emailAddress = %s\n", (parray[p1].emailAddress ?
-		    parray[p1].emailAddress : "(none)"));
-		pprintf(p, "adminLevel = %d\n", parray[p1].adminLevel);
+		pprintf(p, "name           = %s\n", parray[p1].name);
+		pprintf(p, "login          = %s\n", parray[p1].login);
+		pprintf(p, "fullName       = %s\n",
+		    (parray[p1].fullName
+		    ? parray[p1].fullName
+		    : "(none)"));
+		pprintf(p, "emailAddress   = %s\n",
+		    (parray[p1].emailAddress
+		    ? parray[p1].emailAddress
+		    : "(none)"));
+		pprintf(p, "adminLevel     = %d\n", parray[p1].adminLevel);
 #if 0
 		pprintf(p, "network_player = %d\n", parray[p1].network_player);
 #endif
-		pprintf(p, "lastHost = %s\n", dotQuad(parray[p1].lastHost));
-		pprintf(p, "num_comments = %d\n", parray[p1].num_comments);
+		pprintf(p, "lastHost       = %s\n", dotQuad(parray[p1].lastHost));
+		pprintf(p, "num_comments   = %d\n", parray[p1].num_comments);
 
 		player_remove(p1);
 		return COM_OK;
@@ -635,27 +639,31 @@ com_checkPLAYER(int p, param_list param)
 
 		pprintf(p, "%s is number %d in parray of size %d\n", v_player, p1,
 		    (p_num + 1));
-		pprintf(p, "name = %s\n", parray[p1].name);
-		pprintf(p, "login = %s\n", parray[p1].login);
-		pprintf(p, "fullName = %s\n", (parray[p1].fullName ?
-		    parray[p1].fullName : "(none)"));
-		pprintf(p, "emailAddress = %s\n", (parray[p1].emailAddress ?
-		    parray[p1].emailAddress : "(none)"));
-		pprintf(p, "socket = %d\n", parray[p1].socket);
-		pprintf(p, "registered = %d\n", parray[p1].registered);
-		pprintf(p, "last_tell = %d\n", parray[p1].last_tell);
-		pprintf(p, "last_channel = %d\n", parray[p1].last_channel);
-		pprintf(p, "logon_time = %s",
+		pprintf(p, "name           = %s\n", parray[p1].name);
+		pprintf(p, "login          = %s\n", parray[p1].login);
+		pprintf(p, "fullName       = %s\n",
+		    (parray[p1].fullName
+		    ? parray[p1].fullName
+		    : "(none)"));
+		pprintf(p, "emailAddress   = %s\n",
+		    (parray[p1].emailAddress
+		    ? parray[p1].emailAddress
+		    : "(none)"));
+		pprintf(p, "socket         = %d\n", parray[p1].socket);
+		pprintf(p, "registered     = %d\n", parray[p1].registered);
+		pprintf(p, "last_tell      = %d\n", parray[p1].last_tell);
+		pprintf(p, "last_channel   = %d\n", parray[p1].last_channel);
+		pprintf(p, "logon_time     = %s",
 		    (ctime_r(&parray[p1].logon_time, tbuf) != NULL
 		    ? &tbuf[0]
 		    : "n/a"));
-		pprintf(p, "adminLevel = %d\n", parray[p1].adminLevel);
+		pprintf(p, "adminLevel     = %d\n", parray[p1].adminLevel);
 #if 0
 		pprintf(p, "network_player = %d\n", parray[p1].network_player);
 #endif
-		pprintf(p, "thisHost = %s\n", dotQuad(parray[p1].thisHost));
-		pprintf(p, "lastHost = %s\n", dotQuad(parray[p1].lastHost));
-		pprintf(p, "num_comments = %d\n", parray[p1].num_comments);
+		pprintf(p, "thisHost       = %s\n", dotQuad(parray[p1].thisHost));
+		pprintf(p, "lastHost       = %s\n", dotQuad(parray[p1].lastHost));
+		pprintf(p, "num_comments   = %d\n", parray[p1].num_comments);
 	}
 
 	return COM_OK;
