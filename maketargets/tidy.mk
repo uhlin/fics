673205dc6760bbbee2959e4d0c0e5be5041ab559
Markus Uhlin <markus@nifty-networks.net>
2026-03-01 22:43:45 +0100

    More checks

--- maketargets/tidy.mk
+++ maketargets/tidy.mk
@@ -1,7 +1,23 @@
 # The 'tidy' target
 
+CHKS = cert-*,$\
+	-clang-analyzer-security.insecureAPI.strcpy,$\
+	-clang-analyzer-optin.performance.Padding,$\
+	-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling,$\
+	cppcoreguidelines-prefer-member-initializer,$\
+	hicpp-deprecated-headers,$\
+	hicpp-multiway-paths-covered,$\
+	hicpp-special-member-functions,$\
+	hicpp-use-auto,$\
+	hicpp-use-emplace,$\
+	hicpp-use-equals-default,$\
+	hicpp-use-equals-delete,$\
+	hicpp-use-nullptr,$\
+	performance-*,$\
+	portability-*
+
 TIDY = clang-tidy
-TIDYFLAGS = -checks=-clang-analyzer-security.insecureAPI.strcpy,-clang-analyzer-optin.performance.Padding,-clang-analyzer-security.insecureAPI.DeprecatedOrUnsafeBufferHandling -quiet
+TIDYFLAGS = -checks=$(CHKS) -quiet
 FICS_CLANG_TIDYFLAGS ?=
 
 tidy: $(INCLUDE_DIR)ficspaths.h
